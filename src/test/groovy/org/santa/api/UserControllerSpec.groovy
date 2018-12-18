fbc5323e28bc9564390bab0cb39eb7c019676781
Alex Pask <pask200@gmail.com>
2018-12-18 18:09:10 +0000

    card-1 | Added Spock

---
+++ src/test/groovy/org/santa/api/UserControllerSpec.groovy
@@ -0,0 +1,18 @@
+package org.santa.api
+
+import spock.lang.Specification
+
+class UserControllerSpec extends Specification {
+
+
+    def "should"() {
+
+        when:
+
+        1 == 1
+
+        then:
+
+        1 == 1
+    }
+}
