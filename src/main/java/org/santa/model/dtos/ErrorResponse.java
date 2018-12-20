1884e0dddada14e919423daaebe48c19b52ffb4f
Alex Pask <pask200@gmail.com>
2018-12-20 17:10:28 +0000

    card-4 | validation of list

--- src/main/java/org/santa/model/dtos/ErrorResponse.java
+++ src/main/java/org/santa/model/dtos/ErrorResponse.java
@@ -3,9 +3,11 @@ package org.santa.model.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 public class ErrorResponse {
 
-    private String message;
+    private List<String> errors;
 }
